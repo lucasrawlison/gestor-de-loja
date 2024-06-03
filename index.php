3a018facab79c4787431194630e385fd4fa90582
unknown <luc1497@gmail.com>
2024-06-03 16:57:23 -0300

    primeiro update

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="bg">
+        <div class="list-container">
+            <div class="list">
+                <div class="list-header">
+                    <div class="row-container"><span>teste</span></div>
+                    <div class="row-container"><span>teste</span></div>
+                </div>
+                <div class="list-row"></div>
+                <div class="list-row"></div>
+                <div class="list-row"></div>
+                <div class="list-row"></div>
+                <div class="list-row"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
